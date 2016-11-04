88c05c1e6c89d051fc473afd32d33279acd2358f
mjniday <michael.niday@gmail.com>
2016-11-03 22:18:49 -0400

    Removed devise routes so only users can create/edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 end
